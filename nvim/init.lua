88248f783d6482b16319803ffce039802decf2dd
yamazoe.hisatoshi <yamazoe.hisatoshi@mail.canon>
2022-10-31 12:50:29 +0900

    add neovim

---
+++ nvim/init.lua
@@ -0,0 +1,129 @@
+-- packer.nvimを自動でインストール
+local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local packer_bootstrap = nil
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+end
+
+-- プラグインをインストール
+require("packer").startup(function(use)
+  -- パッケージマネージャ
+  use "wbthomason/packer.nvim"
+
+  -- Lsp関係
+  use "neovim/nvim-lspconfig"
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+
+  -- 補完関係
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-vsnip"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/vim-vsnip"
+
+  -- null-ls
+  use { "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } }
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+  
+  -- TreeSitter
+  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+
+  -- テーマ
+  use {"sainnhe/sonokai"}
+
+end)
+
+-- LSPクライアントがバッファにアタッチされたときに実行される
+local on_attach = function(client, bufnr)
+  -- LSPサーバーのフォーマット機能を無効にするには下の行をコメントアウト
+  -- 例えばtypescript-language-serverにはコードのフォーマット機能が付いているが代わりにprettierでフォーマットしたいときなどに使う
+  -- client.resolved_capabilities.document_formatting = false
+
+  local set = vim.keymap.set
+  set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+  set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+  set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+  set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>")
+  set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+end
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach,
+    }
+  end,
+}
+
+-- nvim-cmp(補完) の設定
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local cmp = require "cmp"
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  }, {
+    { name = "buffer" },
+  }),
+}
+
+-- null-ls (formatter/linter)の設定
+local null_ls = require "null-ls"
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.prettier.with {
+      prefer_local = "node_modules/.bin",
+    },
+  },
+}
+
+-- tree-sitterの設定
+require "nvim-treesitter.configs".setup {
+  highlight = {
+    enable = true,
+    disable = {}
+  }
+}
+
+
+vim.opt.number = true
+vim.opt.winblend = 20
+vim.opt.pumblend = 20
+vim.opt.termguicolors = true
+vim.cmd 'colorscheme sonokai'
