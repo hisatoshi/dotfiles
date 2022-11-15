8da9927365512f573d179bdbf30ca26a69ab19cd
yamazoe.hisatoshi <yamazoe.hisatoshi@mail.canon>
2022-11-15 17:09:42 +0900

    weztermの設定ファイルを追加

---
+++ other/.wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", italic=false}),
+  font_size = 13,
+  color_scheme = "Catppuccin Frappe",
+  default_prog = {"wsl.exe", "--distribution", "Ubuntu", "--exec", "/bin/zsh", "-l"},
+  use_ime=true,
+  adjust_window_size_when_changing_font_size = false,
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  window_background_opacity = 0.9,
+  default_cwd = "~"
+}
