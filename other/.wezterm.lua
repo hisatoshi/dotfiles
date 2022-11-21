529c34ecd3c11b550d4b28de89f490b6abe9778f
yamazoe.hisatoshi <yamazoe.hisatoshi@mail.canon>
2022-11-22 08:14:14 +0900

    opacityを設定

--- other/.wezterm.lua
+++ other/.wezterm.lua
@@ -8,6 +8,5 @@ return {
   adjust_window_size_when_changing_font_size = false,
   hide_tab_bar_if_only_one_tab = true,
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-  window_background_opacity = 0.9,
   default_cwd = "~"
 }
